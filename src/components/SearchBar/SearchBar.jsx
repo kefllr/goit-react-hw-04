a12db46a11fea7defb52531256dd00118c77053c
kefllr <timislusar05@gmail.com>
2024-04-19 16:05:30 +0200

    goit-react-hw-04

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,39 @@
+import { Formik, Field, Form } from "formik";
+import toast, { Toaster } from 'react-hot-toast';
+import css from "./SearchBar.module.css"
+
+const notify = () => toast('This field cannot be empty. Please enter a search query');
+
+const FORM_INITIAL_VALUES = {
+    search: "",
+  };
+
+  
+const SearchBar = ({onSearch}) =>{
+    const handleSubmit = (values)=>{
+       onSearch(values.search)
+    }
+    return(
+        <header>
+      <Formik initialValues={FORM_INITIAL_VALUES} onSubmit={handleSubmit}>
+        <Form>
+          <label>
+            <Field
+              className={css.formSearch}
+              type="text"
+              name="search"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+            />
+          </label>
+          <button className={css.formBtn}  type="submit">
+            Search
+          </button>
+          <Toaster />
+        </Form>
+      </Formik>
+    </header>
+    );
+}
+export default SearchBar 
